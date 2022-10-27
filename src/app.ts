6e7dfa77ee4afcd5ffe6c8106e0e78a9a220d128
Scott Garner <scott@j38.net>
2022-10-27 11:14:20 -0700

    Prototype work on 8th wall support.

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,105 @@
-import { url } from 'inspector';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
 
-class MuralScan {
+class App {
+    info: HTMLElement | null;
+    logElement: HTMLElement | null;
 
     scene: THREE.Scene;
     camera: THREE.PerspectiveCamera;
     renderer: THREE.WebGLRenderer;
-    controls: OrbitControls;
 
     mesh: THREE.Mesh;
     image;
     context: CanvasRenderingContext2D;
 
-    constructor() {
-        this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    rootContainer: THREE.Group;
 
-        this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(this.renderer.domElement);
+    selected: THREE.Mesh | null = null;
+
+    initialize(scene, camera, renderer) {
+        this.info = document.getElementById("info");
+        this.logElement = document.getElementById("log");
+        // console.log(this.info)
+        this.info!.hidden = true;
+
+        console.log(scene, camera);
+
+        this.scene = scene;// || new THREE.Scene();
+        this.camera = camera;// || new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        this.renderer = renderer;// new THREE.WebGLRenderer();
+        // this.renderer.setSize(window.innerWidth, window.innerHeight);
+        // document.body.appendChild(this.renderer.domElement);
+
+        this.rootContainer = new THREE.Group();
+        this.scene.add(this.rootContainer);
+
+        this.camera.position.set(0, 1, 0)
 
         const svgUrl = new URL('Rock-of-Ages.svg', import.meta.url);
         const textureUrl = new URL("Rock-of-Ages.jpg", import.meta.url);
 
-        const image = new THREE.ImageLoader().load(svgUrl.toString(), (image) => {
-            console.log(image);
-            this.image = image;
-            const canvas = document.createElement('canvas');
-            canvas.width = this.image.width;
-            canvas.height = this.image.height;
+        const svg = new SVGLoader().load(svgUrl.toString(), (data) => {
 
-            this.context = canvas.getContext('2d')!;
-            this.context!.drawImage(this.image, 0, 0);
+            const paths = data.paths;
+            const container = new THREE.Group();
+            container.scale.multiplyScalar(0.001);
+            container.scale.y *= - 1;
+            container.position.x = -1.6 / 2;
+            container.position.y = 2.16 / 2;
+
+            for (let i = 0; i < paths.length; i++) {
+
+                const path = paths[i];
+                const name = path?.userData?.node?.id;
+
+                console.log(name);
+
+                const material = new THREE.MeshBasicMaterial({
+                    color: new THREE.Color("#ffff00"),// path.color,
+                    side: THREE.DoubleSide,
+                    // depthWrite: false,
+                    transparent: true,
+                    opacity: 0
+                });
+
+                const shapes = SVGLoader.createShapes(path);
+
+                for (let j = 0; j < shapes.length; j++) {
+
+                    const shape = shapes[j];
+                    console.log(shape);
+                    const geometry = new THREE.ShapeGeometry(shape);
+                    const mesh = new THREE.Mesh(geometry, material);
+                    mesh.name = name;
+                    mesh.layers.enable(1);
+                    mesh.position.z = 1 + j;
+                    container.add(mesh);
+
+                }
+
+            }
+
+            this.rootContainer.add(container);
+
         });
+
+        // const image = new THREE.ImageLoader().load(svgUrl.toString(), (image) => {
+
+        //     // Canvas
+
+        //     console.log(image);
+
+        //     this.image = image;
+        //     const canvas = document.createElement('canvas');
+        //     canvas.width = this.image.width;
+        //     canvas.height = this.image.height;
+
+        //     this.context = canvas.getContext('2d')!;
+        //     this.context!.drawImage(this.image, 0, 0);
+
+        // });
 
         const texture = new THREE.TextureLoader().load(textureUrl.toString(), (texture) => {
 
@@ -44,53 +110,93 @@ class MuralScan {
         const planeGeometry = new THREE.PlaneGeometry(1.600, 2.160);
         const planeMaterial = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, map: texture });
         this.mesh = new THREE.Mesh(planeGeometry, planeMaterial);
-        this.scene.add(this.mesh);
+        this.rootContainer.add(this.mesh);
 
 
 
-        this.camera.position.z = 5;
 
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        // this.controls.minPolarAngle = this.controls.maxPolarAngle = Math.PI / 2;
-        // this.controls.enableDamping = true;
-        // this.controls.dampingFactor = 0.05;
-        // this.controls.enablePan = false;
+        // let animate = () => {
+        //     requestAnimationFrame(animate);
 
-        let animate = () => {
-            requestAnimationFrame(animate);
-
-            this.mesh.material.color.set(0xffffff);
-
-            // Raycast
-
-            let raycaster = new THREE.Raycaster();
-            raycaster.setFromCamera(new THREE.Vector2(0, 0), this.camera);
-
-            const intersects = raycaster.intersectObjects(this.scene.children);
-            for (let i = 0; i < intersects.length; i++) {
-
-                let pixel = this.getPixel(intersects[i].uv);
-                if (pixel[0] > 128) {
-                    intersects[i].object.material.color.set(0x00ff00);
-                } else {
-                    intersects[i].object.material.color.set(0xff0000);
-                }
-
-            }
+        // this.mesh.material.color.set(0xffffff);
 
 
-            this.renderer.render(this.scene, this.camera);
-        };
+        // for (let i = 0; i < intersects.length; i++) {
 
-        animate();
+        //     console.log(intersects[i]);
+
+
+        //     // let pixel = this.getPixel(intersects[i].uv);
+        //     // if (pixel[0] > 128) {
+        //     //     intersects[i].object.material.color.set(0x00ff00);
+        //     // } else {
+        //     //     intersects[i].object.material.color.set(0xff0000);
+        //     // }
+        // }
+
+        //     this.renderer.render(this.scene, this.camera);
+        // };
+
+        // animate();
     }
 
-    getPixel(uv: THREE.Vector2) {
-        if (this.context) {
-            return this.context.getImageData(this.image.width * uv.x, this.image.height * uv.y, 1, 1).data;
+    update() {
+        // Raycast
+
+        let raycaster = new THREE.Raycaster();
+        raycaster.setFromCamera(new THREE.Vector2(0, 0), this.camera);
+        raycaster.layers.set(1);
+
+        const intersects = raycaster.intersectObjects(this.scene.children);
+
+        if (this.selected) (this.selected.material as THREE.Material).opacity = 0;
+        this.info!.hidden = true;
+
+        if (intersects.length > 0) {
+            this.selected = intersects[intersects.length - 1].object as THREE.Mesh;
+            this.selected.material!.opacity = .25;
+            this.info!.hidden = false;
+            this.log(intersects[intersects.length - 1].object.name);
+
         }
+
+        this.log("update");
+
+    }
+
+    // getPixel(uv: THREE.Vector2) {
+    //     if (this.context) {
+    //         return this.context.getImageData(this.image.width * uv.x, this.image.height * uv.y, 1, 1).data;
+    //     }
+    // }
+
+
+
+    // 8th Wall
+
+
+    trackingStatus() { }
+    imageLoading() { }
+    imageScanning() { }
+    imageFound() {
+        this.log("FOUND");
+    }
+    imageUpdated({ detail }) {
+
+        this.log(JSON.stringify(detail.position));
+        this.rootContainer.position.copy(detail.position)
+        this.rootContainer.quaternion.copy(detail.rotation)
+        this.rootContainer.scale.set(detail.scale, detail.scale, detail.scale)
+    }
+
+    imageLost() { }
+
+    log(text: string) {
+        const element = document.createElement("div");
+        element.innerText = (text);
+        this.logElement?.prepend(element);
     }
 
 }
 
-export default new MuralScan();
+export default new App();
